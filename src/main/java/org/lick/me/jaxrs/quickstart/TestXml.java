1378d8a7331aab8823a36c75c56a935fd680ca07
Nim <nimlhug@lick-me.org>
2015-02-08 20:33:27 +0100

    Add XML & JSON Bean examples

---
+++ src/main/java/org/lick/me/jaxrs/quickstart/TestXml.java
@@ -0,0 +1,24 @@
+package org.lick.me.jaxrs.quickstart;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Test resource (exposed at "api/testXml" path)
+ */
+@Path("testXml")
+public class TestXml {
+
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public TestData getXml() {
+		final TestData test = new TestData();
+		test.setName("Mega Name");
+		test.setSize("Huge");
+
+		return test;
+	}
+
+}
